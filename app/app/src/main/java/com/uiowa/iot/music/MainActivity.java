57639a15849ff06933910934c0858d8c9003eab2
cnwakama <chibuzo-nwakama@uiowa.edu>
2020-05-04 19:13:46 -0500

    added encryption and connection for socket io

--- app/app/src/main/java/com/uiowa/iot/music/MainActivity.java
+++ app/app/src/main/java/com/uiowa/iot/music/MainActivity.java
@@ -1,27 +1,122 @@
 package com.uiowa.iot.music;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Environment;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URISyntaxException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Socket mSocket;
+
+    private String note;
+    private String encoded;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.main);
+        try {
+            mSocket = IO.socket("https://glimmer-lying-stoplight.glitch.me");
+            mSocket.connect();
+            mSocket.emit("join", "Android");
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        } catch (URISyntaxException ignored) {}
+
+
+        mSocket.on("results", new Emitter.Listener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void call(final Object... args) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        JSONObject data = (JSONObject) args[0];
+//                        String username;
+//                        String message;
+                        try {
+                            note = data.getString("username");
+                            encoded = data.getString("message");
+
+                            Log.i("Decode", "64 bit string");
+                            byte[] decoded = Base64.decode(encoded, 0);
+
+                            File file2 = new File(Environment.getExternalStorageDirectory() + "/hello-5.wav");
+                            FileOutputStream os = new FileOutputStream(file2, true);
+                            os.write(decoded);
+                            os.close();
+
+//                            Utilities.log("~~~~~~~~ Decoded: ", Arrays.toString(decoded));
+
+                            Log.i("Note", note);
+                        } catch (Exception e) {
+                            return;
+                        }
+
+                        // add the message to view
+//                        addMessage(username, message);
+                    }
+                });
+            }
+        });
+
+        mSocket.on("userdisconnect", new Emitter.Listener() {
+            @Override
+            public void call(final Object... args) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String data = (String) args[0];
+
+                        Toast.makeText(MainActivity.this,data,Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mSocket.disconnect();
+        mSocket.off("new message",  new Emitter.Listener() {
+            @Override
+            public void call(final Object... args) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        JSONObject data = (JSONObject) args[0];
+                        String username;
+                        String message;
+                        try {
+                            username = data.getString("username");
+                            message = data.getString("message");
+                        } catch (JSONException e) {
+                            return;
+                        }
+
+                        // add the message to view
+//                        addMessage(username, message);
+                    }
+                });
             }
         });
     }
