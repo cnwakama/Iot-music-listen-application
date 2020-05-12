01748f141eaa72ac97fc403def5ef5a4473e9a17
cnwakama <chibuzo-nwakama@uiowa.edu>
2020-05-12 12:20:40 -0500

    added things for project finalization

--- server/app.js
+++ server/app.js
@@ -2,73 +2,54 @@ const Influx = require('influx');
 var io = require('socket.io-client');
 var socket = io("https://glimmer-lying-stoplight.glitch.me");
 
+const client = new Influx.InfluxDB({
+  database: 'musicDB',
+  // username: 'database',
+  // password: 'root',
+  hosts: [
+    { host: 'localhost' },
+  ],
+  port: 8086,
+});
+
+client.getDatabaseNames()
+  .then(names => {
+    if (!names.includes('musicDB')) {
+      return client.createDatabase('musicDB');
+    }
+  })
+  .then(() => {
+    socket.emit('join', "InfluxDB is setup");
+  })
+  .catch((error) => {
+    console.log( error )
+  });
+
 socket.on('connect', function(){
   console.log('i am connected to the server');
 });
 
-socket.emit('join', "hello world");
-// const influx = new Influx.InfluxDB({
-//   host: 'localhost',
-//   database: 'ocean_tides',
-//   schema: [
-//     {
-//       measurement: 'tide',
-//       fields: { height: Influx.FieldType.FLOAT },
-//       tags: ['unit', 'location']
-//     }
-//   ]
 
-// const client = new InfluxDB({
-//   database: 'musicDB',
-//   username: 'databaase',
-//   password: 'root',
-//   hosts: [
-//     { host: 'localhost' },
-//   ],
-//   port: 8086,
-//   schema: [
-//     {
-//       measurement: 'tide',
-//       fields: { height: Influx.FieldType.FLOAT },
-//       tags: ['unit', 'location']
-//     }
-//   ]
-// });
 
-// influx.getDatabaseNames()
-//   .then(names => {
-//     if (!names.includes('ocean_tides')) {
-//       return influx.createDatabase('ocean_tides');
-//     }
-//   })
-//   .then(() => {
-//     app.listen(app.get('port'), () => {
-//       console.log(`Listening on ${app.get('port')}.`);
-//     });
-//     writeDataToInflux(hanalei);
-//     writeDataToInflux(hilo);
-//     writeDataToInflux(honolulu);
-//     writeDataToInflux(kahului);
-//   })
-//   .catch(error => console.log({ error }));
+socket.on('results', function(data) {
+  console.log(data['note']);
+  client.writePoints([
+  {
+    measurement: 'projectDb',
+    fields: { audio: data['audio'], note: data['note'], duration: data['duration'] },
+  }], {
+    database: 'musicDB',
+  })
+  .catch(error => {
+  console.error(`Error saving data to InfluxDB! ${error.stack}`)
+  });
+});
 
-// influx.writePoints([
-//   {
-//     measurement: 'tide',
-//     tags: {
-//       unit: locationObj.rawtide.tideInfo[0].units,
-//       location: locationObj.rawtide.tideInfo[0].tideSite,
-//     },
-//     fields: { height: tidePoint.height },
-//     timestamp: tidePoint.epoch,
-//   }
-// ], {
-//   database: 'ocean_tides',
-//   precision: 's',
-// })
-// .catch(error => {
-//   console.error(`Error saving data to InfluxDB! ${err.stack}`)
-// });
+
+
+
+
+
 
 // influx.query(`
 //     select * from tide
@@ -77,6 +58,22 @@ socket.emit('join', "hello world");
 //   .then( result => response.status(200).json(result) )
 //   .catch( error => response.status(500).json({ error }) );
 
+// influx.writePoints([
+//   {
+//     measurement: 'response_times',
+//     tags: { host: os.hostname() },
+//     fields: { duration, path: req.path },
+//   }
+// ]).then(() => {
+//   return influx.query(`
+//     select * from response_times
+//     where host = ${Influx.escape.stringLit(os.hostname())}
+//     order by time desc
+//     limit 10
+//   `)
+// }).then(rows => {
+//   rows.forEach(row => console.log(`A request to ${row.path} took ${row.duration}ms`))
+// })
 
 
 // socket.on('connection', (socket) => {
